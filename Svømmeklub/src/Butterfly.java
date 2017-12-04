cd1d349b2d72ba1132ae34368eace284e72e7083
abenhansen <abenhansen@gmail.com>
2017-12-04 10:24:14 +0100

    Delete Butterfly.java

--- Svømmeklub/src/Butterfly.java
+++
@@ -1,9 +0,0 @@
-public class Butterfly extends Disciplin {
-    public Butterfly(int medlemsID, String dato, double træningstid, String stævne, int placering, double stævnetid){
-        super(medlemsID, dato, træningstid, stævne, placering, stævnetid);
-    }
-
-    public Butterfly(int medlemsID, String dato, double træningstid) {
-        super(medlemsID, dato, træningstid);
-    }
-}
