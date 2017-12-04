c854b1e796b8dcc15ea157b43e81fa3c9018308a
abenhansen <abenhansen@gmail.com>
2017-12-04 10:25:11 +0100

    Delete Hundesvømning.java

--- Svømmeklub/src/Hundesvømning.java
+++
@@ -1,9 +0,0 @@
-public class Hundesvømning extends Disciplin {
-    public Hundesvømning(int medlemsID, String dato, double træningstid, String stævne, int placering, double stævnetid){
-        super(medlemsID, dato, træningstid, stævne, placering, stævnetid);
-    }
-
-    public Hundesvømning(int medlemsID, String dato, double træningstid) {
-        super(medlemsID, dato, træningstid);
-    }
-}
